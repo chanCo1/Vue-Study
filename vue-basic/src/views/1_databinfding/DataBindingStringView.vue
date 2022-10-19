8a2fd7ee3c2b88ae2d3688a922393035df745e5a
chanCo <loinsective@naver.com>
2022-10-19 16:27:27 +0900

    test

---
+++ vue-basic/src/views/1_databinfding/DataBindingStringView.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- html 태그는 template 안에 -->
+  <div> <!-- 에러는 아니지만, 감싸줘야 한다. <></>같은 느낌 -->
+    <h1>Hello {{ userName }}!</h1>
+    <p></p>
+  </div>
+</template>
+
+<script>
+// 자바스크립트 코드는 여기에
+export default {
+  data() {
+    return {
+      userName: 'chanCo',
+      message: ''
+    }
+  }
+}
+</script>
+
+<!-- scoped가 있으면 작성된 환경에만 적용됨 -->
+<style scoped>
+/* css는 여기에 */
+</style>
